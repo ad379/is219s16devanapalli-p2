20fce00f762cd8ef9d0629dd1f90a6956e6ebeaa
ad379 <ashd65@gmail.com>
2016-04-01 18:11:29 -0400

    Added files via upload

--- js/gallery.js
+++ js/gallery.js
@@ -44,17 +44,126 @@ function swapPhoto() {
 var mCurrentIndex = 0;
 
 // XMLHttpRequest variable
+var mURL = "images.json";
 var mRequest = new XMLHttpRequest();
+mRequest.onreadystatechange = function() {
+// Do something interesting if file is opened successfully
+if (mRequest.readyState == 4 && mRequest.status == 200) {
+try {
+// Let’s try and see if we can parse JSON (see next slide)
+mJson = JSON.parse(mRequest.responseText);
+// LOOP THROUGH the mJSON array here and fill up the
+// mImages array with GalleryImage objects
+// Let’s print out the JSON; It will likely show as “obj”
+console.log(mJson);
+} catch(err) {
+console.log(err.message)
+}
+}
+};
+mRequest.open("GET",mURL, true);
+mRequest.send();
+
 
 // Array holding GalleryImage objects (see below).
 var mImages = [];
+mImages[].push(new GalleryImage("Dog", "Boar", "Rat", "Ox"));
+mImages[].push(new GalleryImage("Tiger", "Rabbit", "Dragon", "Snake"));
+mImages[].push(new GalleryImage("Horse", "Sheep", "Monkey", "Rooster"));
+console.log(myArray[0].second); //Outputs "Boar"
+console.log(myArray[1].fourth); //Outputs "Snake"
+console.log(myArray[2].first); //Outputs "Horse"
+for (var i = 0; i < myArray.length; i++) {
+console.log(myArray[i].third); // Outputs “"Rat", "Dragon", "Monkey"
+}
 
+{"images": [
+        {
+            "imgPath": "img/places/australia.jpg",
+            "imgLocation": "Australia",
+            "description": "Loch Ard Gorge",
+            "date": "01/01/2016"
+        },
+        {
+            "imgPath": "img/places/austria.jpg",
+            "imgLocation": "Austria",
+            "description": "Austrian chapel",
+            "date": "01/02/2016"
+        },
+        {
+            "imgPath": "img/places/france.jpg",
+            "imgLocation": "Paris",
+            "description": "Eiffel Tower",
+            "date": "01/03/2016"
+        },
+        {
+            "imgPath": "img/places/greece.jpg",
+            "imgLocation": "Greece",
+            "description": "Greek coastline",
+            "date": "01/04/2016"
+        },
+        {
+            "imgPath": "img/places/hungary.jpg",
+            "imgLocation": "Hungary",
+            "description": "Budapest skyline",
+            "date": "01/05/2016"
+        },
+        {
+            "imgPath": "img/places/india.jpg",
+            "imgLocation": "India",
+            "description": "Taj Mahal",
+            "date": "01/06/2016"
+        },
+        {
+            "imgPath": "img/places/italy.jpg",
+            "imgLocation": "Italy",
+            "description": "Venice",
+            "date": "01/07/2016"
+        },
+        {
+            "imgPath": "img/places/japan.jpg",
+            "imgLocation": "Japan",
+            "description": "Hirosaki Castle",
+            "date": "01/08/2016"
+        },
+        {
+            "imgPath": "img/places/korea.jpg",
+            "imgLocation": "South Korea",
+            "description": "Seoul",
+            "date": "01/09/2016"
+        },
+        {
+            "imgPath": "img/places/norway.jpg",
+            "imgLocation": "Norway",
+            "description": "Countryside mountains",
+            "date": "01/10/2016"
+        },
+        {
+            "imgPath": "img/places/switzerland.jpg",
+            "imgLocation": "Switzerland",
+            "description": "Lake castle",
+            "date": "01/11/2016"
+        },
+        {
+            "imgPath": "img/places/thailand.jpg",
+            "imgLocation": "Thailand",
+            "description": "Beachline",
+            "date": "01/12/2016"
+        },
+        {
+            "imgPath": "img/places/turkey.jpg",
+            "imgLocation": "Turkey",
+            "description": "Istanbul",
+            "date": "01/13/2016"
+        }
+  ]
+}
 // Holds the retrived JSON information
 var mJson;
 
 // URL for the JSON to load by default
 // Some options for you are: images.json, images.short.json; you will need to create your own extra.json later
-var mUrl = 'insert_url_here_to_image_json';
+
 
 
 //You can optionally use the following function as your event callback for loading the source of Images from your json data (for HTMLImageObject).
@@ -79,10 +188,9 @@ window.addEventListener('load', function() {
 
 }, false);
 
-function GalleryImage() {
-	//implement me as an object to hold the following data about an image:
-	//1. location where photo was taken
-	//2. description of photo
-	//3. the date when the photo was taken
-	//4. either a String (src URL) or an an HTMLImageObject (bitmap of the photo. https://developer.mozilla.org/en-US/docs/Web/API/HTMLImageElement)
-}
+function GalleryImage(imgPath, imgLocation, description, date) {
+	this.image = imgPath;
+	this.Location = imgLocation;
+	this.description = description;
+	this.date = date;
+};
